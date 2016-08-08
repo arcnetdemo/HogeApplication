88943e7f8765fa87feea072f32aa3060d9a9e426
Akira AKAHORI <akahori@arc-ne.jp>
2016-08-09 01:00:48 +0900

    8/5 講習会分

---
+++ app/src/main/java/jp/arc_net/hogeapplication/Main2Activity.java
@@ -0,0 +1,13 @@
+package jp.arc_net.hogeapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
